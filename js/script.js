90c96fb3f1ca7531b6ca5c214ff626cca3489d86
Brett Ranieri <brett.ranieri@gmail.com>
2022-11-11 14:41:05 -0700

    add comments to describe functionality

--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
 //Add New List Item
 
 function newItem(){
-    let li = $('<li></li>'); 
-    let inputValue = $('#input').val(); 
-    li.append(inputValue); 
+    let li = $('<li></li>'); // creates li
+    let inputValue = $('#input').val(); // targets string entered in input field 
+    li.append(inputValue); // assigns string value to li
 
-    if (inputValue === '') {
-        alert('You can\'t add nothing to the list. You have to write something!'); 
+    if (inputValue === '') { // checks for valid input enrtry
+        alert('You can\'t add nothing to the list. You have to write something!'); // error message
     } else {
-        $('#list').append(li);
+        $('#list').append(li); // adds li to ul
         $('#input').val(''); // clears input after adding li to ul
     }
 
 // Cross Out List Item
 
-    li.each(function (){ 
-        $(this).on('dblclick', function (){
-            $(this).toggleClass("strike");
+    li.each(function (){ // jQuery forEach
+        $(this).on('dblclick', function (){ //on double click
+            $(this).toggleClass("strike"); //toggle cross out
         })
     });
 
 // Delete button
 
-    let crossOutButton = $('<crossOutButton></crossOutButton>');
-    crossOutButton.append(document.createTextNode('X'));
-    li.append(crossOutButton);
+    let crossOutButton = $('<crossOutButton></crossOutButton>'); //creates crossOutButton
+    crossOutButton.append(document.createTextNode('X')); //writes text inside button
+    li.append(crossOutButton); //add button to the li
 
-    crossOutButton.on('click', function (){
+    crossOutButton.on('click', function (){ //delete li on click
         li.fadeOut(500); //used fadeOut animation instead of addClass("delete")
     })
 
 // Sortable functionality
 
-    $('#list').sortable();
+    $('#list').sortable(); //jQuery method to make list sortable.
 
 }
