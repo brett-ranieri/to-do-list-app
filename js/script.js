c16d11b1c91b5c571084f879d47424538c173a85
Brett Ranieri <brett.ranieri@gmail.com>
2022-11-11 13:24:32 -0700

    add cross out functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//Add New List Item
+
 function newItem(){
     let li = $('<li></li>');
     let inputValue = $('#input').val();
@@ -8,4 +10,13 @@ function newItem(){
     } else {
         $('#list').append(li);
     }
+
+// Cross Out List Item
+
+    li.each(function (){
+        $(this).on('dblclick', function (){
+            $(this).toggleClass("strike");
+        })
+    });
+
 }
